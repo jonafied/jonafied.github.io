fbc3debdc7178e8d2c355e9a88bc7542f15aab16
jona agcaoili <jonadesignz@me.com>
2018-02-19 16:24:21 -0800

    scroll to fix

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -16,7 +16,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top +80)
+          scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
         return false;
       }
